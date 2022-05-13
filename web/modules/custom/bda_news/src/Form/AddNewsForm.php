e9b7d13b31973b1af5d510abdf7a428470138c0c
dev081023 <yevhen081023@gmail.com>
2022-05-13 08:26:56 +0300

    created form add news

---
+++ web/modules/custom/bda_news/src/Form/AddNewsForm.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\bda_news\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class AddNewsForm extends FormBase {
+
+  public function getFormId() {
+    return 'bda_add_news_form';
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['title'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Title'),
+      '#markup' => $this->t('Field at least 10 characters long'),
+      '#required' => TRUE,
+    ];
+
+    $form['content'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Content'),
+      '#markup' => $this->t('Field at least 10 characters long'),
+      '#required' => TRUE,
+    ];
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+
+    $title = $form_state->getValue('title');
+    $content = $form_state->getValue('content');
+
+    if (strlen($title) < 10) {
+      // Set an error for the form element with a key of "title".
+      $form_state->setErrorByName('title', $this->t('The title must be at least 10 character long.'));
+    }
+
+    if (strlen($content) < 10) {
+      // Set an error for the form element with a key of "title".
+      $form_state->setErrorByName('title', $this->t('The content must be at least 10 character long.'));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $message = \Drupal::messenger();
+    $message->addMessage('News with id ' . \Drupal::currentUser()->id() . ' was created and now waiting for publishing');
+
+    $form_state->setRedirect('<front>');
+  }
+
+}

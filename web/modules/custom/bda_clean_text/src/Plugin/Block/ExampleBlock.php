26c3eeebd01234b3444c1ce3c01b51d118fb43d9
dev081023 <yevhen081023@gmail.com>
2022-05-29 16:05:52 +0300

    lesson#38 Part.1

---
+++ web/modules/custom/bda_clean_text/src/Plugin/Block/ExampleBlock.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace Drupal\bda_clean_text\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a 'example_block' block.
+ *
+ * @Block(
+ *   id = "example_block",
+ *   admin_label = @Translation("Example block"),
+ *   category = "Beetroot Example"
+ * )
+ */
+class ExampleBlock extends BlockBase {
+
+  /**
+   * @{inheritDoc}
+   */
+  public function defaultConfiguration() {
+    return ['some_config' => ''];
+  }
+
+  /**
+   * @{inheritDoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form['some_config'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Some config'),
+      '#default_value' => $this->configuration['some_config'],
+      '#description' => $this->t('Some text to show in block'),
+      '#weight' => 50,
+    ];
+    return parent::buildConfigurationForm($form, $form_state);
+  }
+
+  /**
+   * @{inheritDoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $this->configuration['some_config'] = $form_state->getValue('some_config');
+    parent::submitConfigurationForm($form, $form_state);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function build() {
+    return [
+      '#markup' => $this->configuration['some_config'],
+    ];
+  }
+
+}

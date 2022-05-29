1ea5f147b061ca196d6c1e35c1fb4eeb6b69300d
dev081023 <yevhen081023@gmail.com>
2022-05-29 12:12:35 +0300

    add phpcs

--- web/modules/custom/beetroot_example/src/Controllers/Example.php
+++ web/modules/custom/beetroot_example/src/Controllers/Example.php
@@ -3,11 +3,13 @@
 namespace Drupal\beetroot_example\Controllers;
 
 use Drupal\Core\Controller\ControllerBase;
-use Laminas\Diactoros\Response\JsonResponse;
 
 class Example extends ControllerBase {
 
   public function view() {
-    return new JsonResponse(['hello' => 'world']);
+    return [
+      '#markup' => 'Hello',
+    ];
   }
+
 }

71830922732ec65c046e82792f2cb8c32ace4008
dev081023 <yevhen081023@gmail.com>
2022-06-01 11:30:13 +0300

    task#40: Batch. No work!

---
+++ web/modules/custom/ex40_batch/src/Form/BatchForm.php
@@ -0,0 +1,87 @@
+<?php
+
+
+namespace Drupal\ex40_batch\Form;
+
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class BatchForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+
+  public function getFormId(): string {
+    return 'ex40_batch_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $storage = \Drupal::entityTypeManager()->getStorage('node');
+    $nodeIds = $storage->getQuery()
+      ->condition('type', 'news')
+      ->condition('field_archive', 0)
+      ->execute();
+    $nodes = $storage->loadMultiple($nodeIds);
+    $output = [];
+    foreach ($nodes as $node) {
+      $output[] = [
+        'id' => $node->id(),
+        'title' => $node->label(),
+      ];
+    }
+    $header = [
+      'title' => $this->t('Node`s title'),
+    ];
+    $form['table'] = [
+      '#type' => 'tableselect',
+      '#header' => $header,
+      '#options' => $output,
+      '#empty' => t('No node`s found'),
+    ];
+    $form['actions'] = [
+      '#type' => 'actions',
+    ];
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $selected = $form_state->getValue('table');
+    $id = $form_state->getValue('table')['options'][0];
+    $operations = [];
+    foreach ($selected as $select) {
+      $operations[] = [
+        '\Drupal\ex40_batch\Form\BatchForm::toArchive',
+        [$id],
+      ];
+    }
+    batch_set([
+      'title' => $this->t('Archive selected News'),
+      'operations' => $operations,
+    ]);
+  }
+
+  public static function toArchive($params) {
+    $nodeStorage = \Drupal::entityTypeManager()->getStorage('node');
+    if ($nodeStorage->getEntityType() == 'news') {
+      $node = $nodeStorage->load($params[0]);
+      $node->set('field_archive', 1);
+      $node->save();
+    }
+  }
+
+}
